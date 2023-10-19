80335181f641b7d17edeffbeff90054dc190d615
ANDyouNo <younofl312w@gmail.com>
2023-10-19 12:09:13 +0300

    Testing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-const hamburger = document.querySelector('.hamburger')
+const hamburger = document.querySelector(".hamburger");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle("is-active")
-})
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  window.scrollTo(scrollX, 0);
+  document.querySelector(".menu").classList.toggle("closed");
+  document.querySelector("body").classList.toggle("scroll-lock");
+  document.querySelectorAll(".menu__link").forEach((link, index) => {
+    console.log(index);
+    if ((index + 1) % 2 === 0) {
+      //even
+      link.classList.toggle("animation-even");
+    } else {
+      //odd
+      link.classList.toggle("animation-odd");
+    }
+  });
+});
